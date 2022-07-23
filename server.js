f92826637aae250520f95e75f7d5144738fcfcc1
PhgX <phuongbm211@gmail.com>
2022-07-23 14:08:23 +0700

    Connect to MySQL done

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 const http = require('http');
+const mysql = require('mysql');
+
+let configToMySQL = {
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'project3',
+    charset: 'utf8mb4'
+}
+
+const connection = mysql.createConnection(configToMySQL);
+connection.connect((err) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log('Connect Sucess');
+    }
+});
+
 
 const server = http.createServer((req, res) => {
 
